9c94dbafaa703e870c5399fe0ac17cc7e16a2b83
Ryan Poplawski <claydewd@gmail.com>
2015-09-28 18:02:41 -0500

    a bunch new files; initial commit

---
+++ db/migrate/20150928215942_create_students.rb
@@ -0,0 +1,9 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :student_name
+      t.belongs_to :student, index: true, foreign_key: true
+      t.timestamps null: false
+    end
+  end
+end
