1afa8b305b74ec4e9103a8a03c0471035ac5299a
Ryan Poplawski <claydewd@gmail.com>
2015-09-28 22:41:26 -0500

    fixed typo in controller of teacher

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,11 +1,11 @@
 class TeachersController < ApplicationControlteacer
   def index
-    teachers = User.all
+    teachers = Teacher.all
     render template: 'teachers/index.html.erb', locals: { teachers: teachers }
   end
 
   def show
-    teacher = User.find(params[:id])
+    teacher = Teacher.find(params[:id])
     render template: 'teachers/show.html.erb', locals: { teacher: teacher }
   end
 end
